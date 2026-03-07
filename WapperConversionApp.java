2ed8f0748f905d78ffb96a466c7cf90b4dfada43
GauravBhoi <bhoigaurav432@gmail.com>
2026-03-07 22:54:56 +0530

    Wapper class app

---
+++ WapperConversionApp.java
@@ -0,0 +1,14 @@
+//There are two type of conversion Primetive And Referencial Convertion 
+class WapperConversionApp
+{
+     public static void main(String args[])
+     {
+          int a = 10;
+          long b =a;
+          System.out.println("I AM Implicit Converstion  "+b);
+ 
+         long c=20;
+         int d=(int)c;
+         System.out.println("I Am Explicit Conversion  "+d);
+    }
+}
