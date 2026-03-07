6a5f983db48c80378e170494b38c5cf1420cfd9e
GauravBhoi <bhoigaurav432@gmail.com>
2026-03-07 22:58:42 +0530

    AutoBoxing And AutounBoxing In Java

---
+++ AutoBoxingAndAutounboxing.java
@@ -0,0 +1,13 @@
+// Wapper Class Autoboxing And AutounBoxing but we are perform only numeric type of data 
+class AutoBoxingAndAutounboxing
+{
+     public static void main(String args[])
+     {
+          int a=10;
+          Integer b=a;
+          System.out.println(" Primetive to refrence type of data "+b);
+       
+          int c=b;
+          System.out.println(" Refrence to primetive type of data "+c);
+     }
+}
